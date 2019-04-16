102fd7654cec8d056130cd390e9d47f9f605d012
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-04-16 17:00:13 +0300

    Adding Basic web exercise

---
+++ 10BasicWeb/02BasicWeb-Exercise/Calculator-Completed/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+            return RedirectToAction("Index", calculator);
+        }
+
+    }
+}
