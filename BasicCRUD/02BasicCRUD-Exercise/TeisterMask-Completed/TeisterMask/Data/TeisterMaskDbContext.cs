66df4e0e75333e14812e16f194e1f02744559eb0
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-04-17 08:35:26 +0300

    Adding Basic CRUD exercise

---
+++ BasicCRUD/02BasicCRUD-Exercise/TeisterMask-Completed/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext:DbContext
+    {
+ 
+        public DbSet<Task> Tasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SQLEXPRESS; Database = TeisterMaskDb1; Integrated Security = True");
+
+        }
+
+    }
+}
