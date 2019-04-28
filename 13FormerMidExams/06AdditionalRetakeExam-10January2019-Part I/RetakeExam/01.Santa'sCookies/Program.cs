a0a84731ed49393640cd1ac495d78ffdcb623a86
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-04-28 14:43:39 +0300

    Adding Mid Exam exercises from 10Jan2019

---
+++ 13FormerMidExams/06AdditionalRetakeExam-10January2019-Part I/RetakeExam/01.Santa'sCookies/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01Santa_sCookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int batchesCount = int.Parse(Console.ReadLine());
+            int totalBoxes = 0;
+            for (int i = 0; i < batchesCount; i++)
+            {
+                double flourGr = double.Parse(Console.ReadLine());
+                double sugarGr = double.Parse(Console.ReadLine());
+                double cocoaGr = double.Parse(Console.ReadLine());
+
+
+                int flourCups = (int)Math.Floor(flourGr / 140.0);
+                int sugarSpoons = (int)Math.Floor(sugarGr / 20.0);
+                int cocoaSpoons = (int)Math.Floor(cocoaGr / 10.0);
+
+                if (flourCups <= 0 || sugarSpoons <= 0 || cocoaSpoons <= 0)
+                {
+                    Console.WriteLine("Ingredients are not enough for a box of cookies.");
+                    continue;
+                }
+                else
+                {
+                    int minTemp = int.MaxValue;
+                    if (flourCups <= sugarSpoons)
+                    {
+                        minTemp = flourCups;
+                    }
+                    else
+                    {
+                        minTemp = sugarSpoons;
+                    }
+                    double dozi = Math.Min(minTemp, cocoaSpoons);
+                    double totalCookies = dozi * (170 / 25.0);
+
+                    double boxesPerBatch = Math.Floor(totalCookies / 5.0);
+
+                    totalBoxes += (int)boxesPerBatch;
+                    Console.WriteLine($"Boxes of cookies: {boxesPerBatch}");
+                }
+            }
+            Console.WriteLine($"Total boxes: {totalBoxes}");
+        }
+    }
+}
---
+++ 13FormerMidExams/06AdditionalRetakeExam-10January2019-Part I/RetakeExam/02.Santa'sGifts/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02Santa_sGifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandsCount = int.Parse(Console.ReadLine());
+            List<int> housesList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int currentIndex = 0;
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                if (command[0] == "Forward")
+                {
+                    int stepsAhead = int.Parse(command[1]);
+                    if (currentIndex + stepsAhead < housesList.Count)
+                    {
+                        currentIndex += stepsAhead;
+                        housesList.RemoveAt(currentIndex);
+                    }
+                }
+                else if (command[0] == "Back")
+                {
+                    int stepsBack = int.Parse(command[1]);
+                    if (currentIndex - stepsBack >= 0)
+                    {
+                        currentIndex -= stepsBack;
+                        housesList.RemoveAt(currentIndex);
+                    }
+                }
+                else if (command[0] == "Gift")
+                {
+                    int indexToInsertAt = int.Parse(command[1]);
+                    int elementToInsert = int.Parse(command[2]);
+                    if (indexToInsertAt >= 0 && indexToInsertAt < housesList.Count)
+                    {
+                        housesList.Insert(indexToInsertAt, elementToInsert);
+                        currentIndex = indexToInsertAt;
+                    }
+                }
+                else if (command[0] == "Swap")
+                {
+                    int first = int.Parse(command[1]);
+                    int second = int.Parse(command[2]);
+                    if (housesList.Contains(first) && housesList.Contains(second))
+                    {
+                        int firstIndex = housesList.IndexOf(first);
+                        int secondIndex = housesList.IndexOf(second);
+                        housesList.RemoveAt(firstIndex);
+                        housesList.Insert(firstIndex, second);
+                        housesList.RemoveAt(secondIndex);
+                        housesList.Insert(secondIndex, first);
+                    }
+                }
+            }
+            Console.WriteLine($"Position: {currentIndex}");
+            Console.WriteLine(string.Join(", ", housesList));
+        }
+    }
+}
