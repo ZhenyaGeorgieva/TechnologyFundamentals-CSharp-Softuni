eb3aaeafe753428c7c0365c697189ad8721ac626
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-04-28 18:06:59 +0300

    Adding Mid exam exercises

---
+++ 13FormerMidExams/07AdditionalRetakeExam-28October2018-Part I/AddRetakeExam/01.GoogleSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01GoogleSearches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int users = int.Parse(Console.ReadLine());
+            double moneyPerSearch = double.Parse(Console.ReadLine());
+
+            double totalMoney = 0;
+            for (int i = 1; i <= users; i++)
+            {
+                double wordsCount = double.Parse(Console.ReadLine());
+                double moneyPerThisUser = moneyPerSearch;
+                if (wordsCount > 5)
+                {
+                    continue;
+                }
+                if (wordsCount == 1)
+                {
+                    moneyPerThisUser = moneyPerSearch * 2;
+                }
+                if (i % 3 == 0)
+                {
+                    moneyPerThisUser *= 3;
+                }
+                totalMoney += moneyPerThisUser;
+            }
+            totalMoney *= days;
+            Console.WriteLine($"Total money earned for {days} days: {totalMoney:f2}");
+        }
+    }
+}
---
+++ 13FormerMidExams/07AdditionalRetakeExam-28October2018-Part I/AddRetakeExam/02.ChatLogger/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                List<string> tokens = command.Split().ToList();
+                string action = tokens[0];
+                if (action == "Chat")
+                {
+                    list.Add(tokens[1]);
+                }
+                else if (action == "Delete")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == tokens[1])
+                        {
+                            list.Remove(tokens[1]);
+                        }
+                    }
+                }
+                else if (action == "Edit")
+                {
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == tokens[1])
+                        {
+                            list[i] = list[i].Replace(tokens[1], tokens[2]);
+                        }
+                    }
+                }
+                else if (action == "Pin")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == tokens[1])
+                        {
+                            list.Add(tokens[1]);
+                            list.Remove(tokens[1]);
+                        }
+                    }
+                }
+                else if (action == "Spam")
+                {
+                    for (int i = 1; i < tokens.Count; i++)
+                    {
+                        list.Add(tokens[i]);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var x in list)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
