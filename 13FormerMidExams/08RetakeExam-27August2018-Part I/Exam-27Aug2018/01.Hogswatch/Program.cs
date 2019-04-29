6b64c2541c007f46d654aa4579c24b97b5178fc7
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-04-29 15:19:04 +0300

    Adding Mid Exam from 27 August 2018

---
+++ 13FormerMidExams/08RetakeExam-27August2018-Part I/Exam-27Aug2018/01.Hogswatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _01Hogswatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int homes = int.Parse(Console.ReadLine());
+            int presentsCount = int.Parse(Console.ReadLine());
+            int initialPresents = presentsCount;
+            int visitedHomes = 0;
+            int additionalPresentsTaken = 0;
+            int timesBack = 0;
+            int totalAdditional = 0;
+            for (int i = 0; i < homes; i++)
+            {
+                int presentsNeeded = int.Parse(Console.ReadLine());
+                visitedHomes++;
+                if (presentsCount >= presentsNeeded)
+                {
+                    presentsCount -= presentsNeeded;
+                }
+                else
+                {
+                    timesBack++;
+                    additionalPresentsTaken = (initialPresents / visitedHomes) * (homes - visitedHomes) + (presentsNeeded - presentsCount);
+                    totalAdditional += additionalPresentsTaken;
+                    presentsCount += additionalPresentsTaken;
+                    presentsCount -= presentsNeeded;
+                }
+            }
+            if (timesBack == 0)
+            {
+                Console.WriteLine($"{presentsCount}");
+            }
+            else
+            {
+                Console.WriteLine($"{timesBack}");
+                Console.WriteLine($"{totalAdditional}");
+            }
+        }
+    }
+}
---
+++ 13FormerMidExams/08RetakeExam-27August2018-Part I/Exam-27Aug2018/02.Grains of Sands/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02GrainsOfSand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> grainsOfSand = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            while (command != "Mort")
+            {
+                List<string> tokens = command.Split().ToList();
+                string action = tokens[0];
+                if (action == "Add")
+                {
+                    int valueToAdd = int.Parse(tokens[1]);
+                    grainsOfSand.Add(valueToAdd);
+                }
+                else if (action == "Remove")
+                {
+                    int value = int.Parse(tokens[1]);
+                    if (grainsOfSand.Contains(value))
+                    {
+                        grainsOfSand.Remove(value);
+                    }
+                    else if (value >= 0 && value < grainsOfSand.Count)
+                    {
+                        grainsOfSand.RemoveAt(value);
+                    }
+                }
+                else if (action == "Replace")
+                {
+                    int value = int.Parse(tokens[1]);
+                    int replacement = int.Parse(tokens[2]);
+                    if (grainsOfSand.Contains(value))
+                    {
+                        int indexOfValue = grainsOfSand.IndexOf(value);
+                        grainsOfSand.RemoveAt(indexOfValue);
+                        grainsOfSand.Insert(indexOfValue, replacement);
+                    }
+                }
+                else if (action == "Increase")
+                {
+                    int value = int.Parse(tokens[1]);
+                    int increaseValue = int.MinValue;
+                    bool increaseValueFound = false;
+                    for (int i = 0; i < grainsOfSand.Count; i++)
+                    {
+                        if (grainsOfSand[i] >= value)
+                        {
+                            increaseValue = grainsOfSand[i];
+                            increaseValueFound = true;
+                            break;
+                        }
+                    }
+                    if (!increaseValueFound)
+                    {
+                        increaseValue = grainsOfSand[grainsOfSand.Count - 1];
+                    }
+                    for (int i = 0; i < grainsOfSand.Count; i++)
+                    {
+                        grainsOfSand[i] += increaseValue;
+                    }
+                }
+                else if (action == "Collapse")
+                {
+                    int value = int.Parse(tokens[1]);
+                    grainsOfSand = grainsOfSand.Where(x => x >= value).Select(x => x).ToList();
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", grainsOfSand));
+        }
+    }
+}
