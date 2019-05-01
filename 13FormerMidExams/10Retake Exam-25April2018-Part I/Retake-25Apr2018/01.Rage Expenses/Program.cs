fd6c142931890ad24c651088cb77ae8bc8f9ef46
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-01 10:39:21 +0300

    Add Mid exam from 25 Apr 2018

---
+++ 13FormerMidExams/10Retake Exam-25April2018-Part I/Retake-25Apr2018/01.Rage Expenses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headSetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            int brokenKeyboards = 0;
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    totalPrice += headSetPrice;
+                }
+                if (i % 3 == 0)
+                {
+                    totalPrice += mousePrice;
+                }
+                if (i % 6 == 0)
+                {
+                    totalPrice += keyboardPrice;
+                    brokenKeyboards++;
+                    if (brokenKeyboards % 2 == 0)
+                    {
+                        totalPrice += displayPrice;
+                    }
+                }
+            }
+            Console.WriteLine($"Rage expenses: {totalPrice:f2} lv.");
+        }
+    }
+}
