7295c6545dfae3d1123381361ff977dc68152d9a
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-01 10:45:18 +0300

    Adding Mid exam from 04 Mar 2018

---
+++ 13FormerMidExams/11Exam-04March2018-Part I/Exam-04Mar2018/01.Padawan Equipment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double priceLightSabres = double.Parse(Console.ReadLine());
+            double priceRobes = double.Parse(Console.ReadLine());
+            double priceBelts = double.Parse(Console.ReadLine());
+
+            double quantityLightSabres = Math.Ceiling(1.1 * studentsCount);
+            int freeBelts = studentsCount / 6;
+            double totalPrice = quantityLightSabres * priceLightSabres + studentsCount * priceRobes + studentsCount * priceBelts - freeBelts * priceBelts;
+            if (totalPrice <= budget)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {totalPrice - budget:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ 13FormerMidExams/11Exam-04March2018-Part I/Exam-04Mar2018/02.Kamino Factory/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfArray = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int bestDNAnumber = int.MaxValue;
+            int bestDNAsum = int.MinValue;
+            int bestDNASequence = int.MinValue;
+            int[] bestDNA = new int[lengthOfArray];
+            int smallestStartingIndex = int.MaxValue;
+            int currentDNAnumber = 0;
+            while (command != "Clone them!")
+            {
+                int[] currentDNA = command
+                    .Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int currentDNASequence = 0;
+                int currentDNAsum = 0;
+                currentDNAnumber++; ;
+                int currentBest = int.MinValue;
+                int startingIndex = -1;
+                for (int i = 0; i < currentDNA.Length - 1; i++)
+                {
+                    if (currentDNA[i] == currentDNA[i + 1] && currentDNA[i] == 1)
+                    {
+                        currentDNASequence++;
+                    }
+                    else
+                    {
+                        currentDNASequence = 0;
+                    }
+                    currentDNAsum += currentDNA[i];
+
+                    if (currentDNASequence > currentBest)
+                    {
+                        currentBest = currentDNASequence;
+                        startingIndex = i;
+                    }
+                }
+                currentDNAsum += currentDNA[lengthOfArray - 1];
+                if (currentBest > bestDNASequence)
+                {
+                    bestDNASequence = currentBest;
+                    bestDNAsum = currentDNAsum;
+                    bestDNAnumber = currentDNAnumber;
+                    smallestStartingIndex = startingIndex;
+                    bestDNA = currentDNA;
+                }
+                else if (currentBest == bestDNASequence
+                    && startingIndex < smallestStartingIndex)
+                {
+                    bestDNASequence = currentBest;
+                    bestDNAsum = currentDNAsum;
+                    bestDNAnumber = currentDNAnumber;
+                    smallestStartingIndex = startingIndex;
+                    bestDNA = currentDNA;
+                }
+                else if (currentBest == bestDNASequence
+                    && startingIndex == smallestStartingIndex
+                    && currentDNAsum > bestDNAsum)
+                {
+                    bestDNASequence = currentBest;
+                    bestDNAsum = currentDNAsum;
+                    bestDNAnumber = currentDNAnumber;
+                    smallestStartingIndex = startingIndex;
+                    bestDNA = currentDNA;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Best DNA sample {bestDNAnumber} with sum: {bestDNAsum}.");
+            Console.WriteLine(string.Join(" ", bestDNA));
+        }
+    }
+}
