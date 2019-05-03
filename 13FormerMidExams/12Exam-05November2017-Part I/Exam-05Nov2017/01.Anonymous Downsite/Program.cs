3d542eb22e4b2bbcd50d561c69a58729012ba234
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-03 15:32:11 +0300

    Adding Mid exam exercises from 05 Nov 2017

---
+++ 13FormerMidExams/12Exam-05November2017-Part I/Exam-05Nov2017/01.Anonymous Downsite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01AnonimousDownside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int websitesCount = int.Parse(Console.ReadLine());
+            BigInteger securityKey = BigInteger.Parse(Console.ReadLine());
+            decimal totalLoss = 0;
+            List<string> websiteList = new List<string>();
+            for (int i = 0; i < websitesCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split().ToArray();
+                string websiteName = tokens[0];
+                long visits = long.Parse(tokens[1]);
+                decimal price = decimal.Parse(tokens[2]);
+
+                decimal siteLoss = visits * price;
+                totalLoss += siteLoss;
+                websiteList.Add(websiteName);
+            }
+            foreach (var x in websiteList)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine($"Total Loss: { totalLoss:f20}");
+            BigInteger securityPrint = BigInteger.Pow(securityKey, websitesCount);
+            Console.WriteLine($"Security Token: {securityPrint}");
+        }
+    }
+}
---
+++ 13FormerMidExams/12Exam-05November2017-Part I/Exam-05Nov2017/02.Anonymous Threat/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02AnonimousTreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+     
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "3:1")
+                {
+                    break;
+                }
+                else
+                {
+
+                    string[] tokens = command.Split().ToArray();
+                    string action = tokens[0];
+                    if (action == "merge")
+                    {
+                        int startIndex = int.Parse(tokens[1]);
+                        int endIndex = int.Parse(tokens[2]);
+                        string concatWord = string.Empty;
+                        if (endIndex > names.Count - 1 || endIndex < 0)
+                        {
+                            endIndex = names.Count - 1;
+                        }
+                        if (startIndex < 0 || startIndex > names.Count)
+                        {
+                            startIndex = 0;
+                        }
+                        for (int i = startIndex; i <= endIndex; i++)
+                        {
+                            concatWord += names[i];
+                        }
+                        names.RemoveRange(startIndex, endIndex - startIndex + 1);
+                        names.Insert(startIndex, concatWord);
+                    }
+                    else if (action == "divide")
+                    {
+                        int index = int.Parse(tokens[1]);
+                        int partitions = int.Parse(tokens[2]);
+                        string wordToDivide = names[index];
+                        int lengthOfWord = wordToDivide.Length;
+                        int lengthOfSubstring = lengthOfWord / partitions;
+                        List<string> substrings = new List<string>();
+                        int i = 0;
+                        for (i = 0; i < partitions; i++)
+                        {
+                            if (i == partitions - 1)
+                            {
+                                substrings.Add(wordToDivide.Substring(i * lengthOfSubstring));
+                            }
+                            else
+                            {
+                                substrings.Add(wordToDivide.Substring(i * lengthOfSubstring, lengthOfSubstring));
+                            }
+                        }
+                        names.RemoveAt(index);
+                        names.InsertRange(index, substrings);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", names));
+        }
+    }
+}
