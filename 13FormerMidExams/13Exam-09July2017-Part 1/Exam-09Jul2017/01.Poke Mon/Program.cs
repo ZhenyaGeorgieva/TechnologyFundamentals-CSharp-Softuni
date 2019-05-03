77c9c247260e6d84963f44db315a496b780bbe15
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-03 15:41:02 +0300

    Adding Mid exam exercises from 09 Jul 2017

---
+++ 13FormerMidExams/13Exam-09July2017-Part 1/Exam-09Jul2017/01.Poke Mon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int powerN = int.Parse(Console.ReadLine());
+            int originalValueN = powerN;
+            decimal halfOfOriginal = originalValueN / 2.0m;
+            int distanceM = int.Parse(Console.ReadLine());
+            int exhauseFactorY = int.Parse(Console.ReadLine());
+            int targets = 0;
+
+            while (powerN >= distanceM)
+            {
+                powerN -= distanceM;
+                targets++;
+                if (powerN == halfOfOriginal)
+                {
+                    if (exhauseFactorY != 0)
+                    {
+                        powerN /= exhauseFactorY;
+                    }
+                }
+            }
+            Console.WriteLine(powerN);
+            Console.WriteLine(targets);
+        }
+    }
+}
---
+++ 13FormerMidExams/13Exam-09July2017-Part 1/Exam-09Jul2017/02Pokemon Don't go/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02PokeMonDontGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pokemons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int sum = 0;
+            while (pokemons.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                int elementToRemove = int.MaxValue;
+                if (index < 0)
+                {
+                    elementToRemove = pokemons[0];
+                    sum += elementToRemove;
+                    pokemons.RemoveAt(0);
+                    int elementToInsert = pokemons[pokemons.Count - 1];
+                    pokemons.Insert(0, elementToInsert)
+;
+                }
+                else if (index > pokemons.Count - 1)
+                {
+                    elementToRemove = pokemons[pokemons.Count - 1];
+                    sum += elementToRemove;
+                    pokemons.RemoveAt(pokemons.Count - 1);
+                    int elementToInsert = pokemons[0];
+                    pokemons.Add(elementToInsert);
+                }
+                else
+                {
+                    elementToRemove = pokemons[index];
+                    sum += elementToRemove;
+                    pokemons.RemoveAt(index);
+                }
+                for (int i = 0; i < pokemons.Count; i++)
+                {
+                    if (pokemons[i] <= elementToRemove)
+                    {
+                        pokemons[i] += elementToRemove;
+                    }
+                    else
+                    {
+                        pokemons[i] -= elementToRemove;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
