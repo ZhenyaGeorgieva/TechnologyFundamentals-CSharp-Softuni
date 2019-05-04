8d19fe8e8191d93a4f8ae50a1c881edf92b22e2d
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-04 18:46:47 +0300

    Add Mid exam exercises from 23October2016

---
+++ 13FormerMidExams/14Exam-23October2016-Part 1/Exam-23Oct2016/01.Charity Marathon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01CharityMarathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double runnersCount = double.Parse(Console.ReadLine());
+            double averageLapsPerRunner = double.Parse(Console.ReadLine());
+            double lengthTrack = double.Parse(Console.ReadLine());
+            double capacityOfTrack = double.Parse(Console.ReadLine());
+            double moneyPerKm = double.Parse(Console.ReadLine());
+            double totalCapacity = capacityOfTrack * days;
+            if (runnersCount > totalCapacity)
+            {
+                runnersCount = totalCapacity;
+            }
+            double metersTotal = runnersCount * averageLapsPerRunner * lengthTrack;
+            double kmTotal = metersTotal / 1000.0;
+            double totalMoney = kmTotal * moneyPerKm;
+            Console.WriteLine($"Money raised: {totalMoney:f2}");
+        }
+    }
+}
---
+++ 13FormerMidExams/14Exam-23October2016-Part 1/Exam-23Oct2016/02.Ladybugs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] positionsOfLadybugs =
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] field = new int[fieldSize];
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int j = 0; j < positionsOfLadybugs.Length; j++)
+                {
+                    if (i == positionsOfLadybugs[j])
+                    {
+                        field[i] = 1;
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string[] givenDirections =
+                    command
+                    .Split()
+                    .ToArray();
+
+                int indexOfLadybug = int.Parse(givenDirections[0]);
+                string directionOfFlight = givenDirections[1];
+                int lengthOfFlight = int.Parse(givenDirections[2]);
+                bool thereIsALadybugHere = true;
+
+                while (indexOfLadybug >= 0 && indexOfLadybug < fieldSize && field[indexOfLadybug] == 1)
+                {
+                    if (thereIsALadybugHere)
+                    {
+                        thereIsALadybugHere = false;
+                        field[indexOfLadybug] = 0;
+                    }
+                    if (directionOfFlight == "left")
+                    {
+                        indexOfLadybug -= lengthOfFlight;
+                        if (indexOfLadybug >= 0 && indexOfLadybug < fieldSize && field[indexOfLadybug] == 0)
+                        {
+                            field[indexOfLadybug] = 1;
+                            break;
+                        }
+                    }
+                    if (directionOfFlight == "right")
+                    {
+                        indexOfLadybug += lengthOfFlight;
+                        if (indexOfLadybug >= 0 && indexOfLadybug < fieldSize && field[indexOfLadybug] == 0)
+                        {
+                            field[indexOfLadybug] = 1;
+                            break;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
