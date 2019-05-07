b1f8b270c675e429befd346ed8a0e811525b3634
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-07 09:06:13 +0300

    Adding Final exam exercises from 16 Dec 2018

---
+++ 14FormerFinalExams/01Final Exam-16December2018/Final Exam-16Dec2018/01.Concert/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> nameMembers = new Dictionary<string, List<string>>();
+            Dictionary<string, int> nameTime = new Dictionary<string, int>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "start of concert")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] tokens = command.Split("; ").ToArray();
+                    string action = tokens[0].Trim();
+                    if (action == "Add")
+                    {
+                        string name = tokens[1].Trim();
+                        if (!nameMembers.ContainsKey(name))
+                        {
+                            nameMembers[name] = new List<string>();
+                        }
+                        if (!nameTime.ContainsKey(name))
+                        {
+                            nameTime[name] = 0;
+                        }
+                        string[] members = tokens[2].Split(", ").ToArray();
+                        for (int i = 0; i < members.Length; i++)
+                        {
+                            string member = members[i].Trim();
+                            if (!nameMembers[name].Contains(member)
+                                && nameMembers.Values.Any(x => x.Contains(member) == false))
+                            {
+                                nameMembers[name].Add(member);
+                            }
+                        }
+                    }
+                    if (action == "Play")
+                    {
+                        string name = tokens[1].Trim();
+                        int time = int.Parse(tokens[2].Trim());
+
+                        if (!nameTime.ContainsKey(name))
+                        {
+                            nameTime[name] = time;
+                        }
+                        else
+                        {
+                            nameTime[name] += time;
+                        }
+                        if (!nameMembers.ContainsKey(name))
+                        {
+                            nameMembers[name] = new List<string>();
+                        }
+                    }
+                }
+            }
+            int totalTime = nameTime.Values.Sum();
+            Console.WriteLine($"Total time: {totalTime}");
+            foreach (var kvp in nameTime.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                int time = kvp.Value;
+                Console.WriteLine($"{name} -> {time}");
+            }
+            string bandToPrint = Console.ReadLine();
+            Console.WriteLine(bandToPrint);
+            foreach (var x in nameMembers[bandToPrint])
+            {
+                Console.WriteLine($"=> {x}");
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/01Final Exam-16December2018/Final Exam-16Dec2018/02.Song Encryption/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02SongEnscryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string artistSongPattern = @"^(?<artist>[A-Z]{1}[a-z\'\s]{1,}):(?<song>[A-Z\s]{1,})$";
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (text == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    if (Regex.IsMatch(text, artistSongPattern))
+                    {
+                        var artistSongCollection = Regex.Matches(text, artistSongPattern);
+                        string artist = string.Empty;
+                        string song = string.Empty;
+                        StringBuilder sb = new StringBuilder();
+                        foreach (Match item in artistSongCollection)
+                        {
+                            artist = item.Groups["artist"].Value.ToString();
+                            song = item.Groups["song"].Value.ToString();
+                            int key = artist.Length;
+                            for (int i = 0; i < artist.Length; i++)
+                            {
+                                char symbol = artist[i];
+
+                                if (symbol == ' ')
+                                {
+                                    sb.Append(" ");
+                                }
+                                else if (symbol == '\'')
+                                {
+                                    sb.Append("'");
+                                }
+                                else
+                                {
+                                    int asciiCode = (int)symbol;
+                                    int asciiCodeKey = asciiCode + key;
+                                    if (key + asciiCode >= 65
+                                        && key + asciiCode <= 90
+                                        && char.IsUpper(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode > 90
+                                        && char.IsUpper(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode - 26);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode >= 97
+                                        && key + asciiCode <= 122
+                                        && char.IsLower(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode > 122
+                                        && char.IsLower(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode - 26);
+                                        sb.Append(newSymbol);
+                                    }
+                                }
+                            }
+                            sb.Append("@");
+                            for (int j = 0; j < song.Length; j++)
+                            {
+                                char symbol = song[j];
+                                if (symbol == ' ')
+                                {
+                                    sb.Append(" ");
+                                }
+                                else
+                                {
+                                    int asciiCode = (int)symbol;
+                                    if (key + asciiCode >= 65
+                                        && key + asciiCode <= 90
+                                        && char.IsUpper(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode > 90
+                                        && char.IsUpper(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode - 26);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode >= 97
+                                        && key + asciiCode <= 122
+                                        && char.IsLower(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode);
+                                        sb.Append(newSymbol);
+                                    }
+                                    if (key + asciiCode > 122
+                                        && char.IsLower(symbol))
+                                    {
+                                        char newSymbol = (char)(key + asciiCode - 26);
+                                        sb.Append(newSymbol);
+                                    }
+                                }
+                            }
+                        }
+                        Console.WriteLine($"Successful encryption: {sb.ToString()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input!");
+                    }
+                }
+            }
+        }
+    }
+}
