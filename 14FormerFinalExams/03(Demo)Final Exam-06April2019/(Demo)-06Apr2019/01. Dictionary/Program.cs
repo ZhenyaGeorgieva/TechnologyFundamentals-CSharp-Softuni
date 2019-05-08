523cba9e586a28ee4a12ddbf0a89170535edcebc
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-08 10:25:05 +0300

    Add Demo Final Exam - 06 April 2019

---
+++ 14FormerFinalExams/03(Demo)Final Exam-06April2019/(Demo)-06Apr2019/01. Dictionary/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> wordsDefinitions = new Dictionary<string, List<string>>();
+            string[] tokens = Console.ReadLine().Split(" | ");
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string[] wordAndDefTokens = tokens[i].Split(": ");
+                string word = wordAndDefTokens[0];
+                string definition = wordAndDefTokens[1];
+                if (!wordsDefinitions.ContainsKey(word))
+                {
+                    wordsDefinitions[word] = new List<string>();
+                    wordsDefinitions[word].Add(definition);
+                }
+                else
+                {
+                    wordsDefinitions[word].Add(definition);
+                }
+            }
+            string[] wordsToSearch = Console.ReadLine().Split(" | ");
+            for (int i = 0; i < wordsToSearch.Length; i++)
+            {
+                string wordToSearchInDictionary = wordsToSearch[i];
+                if (wordsDefinitions.ContainsKey(wordToSearchInDictionary))
+                {
+                    Console.WriteLine($"{wordToSearchInDictionary}");
+
+                    foreach (var item in wordsDefinitions[wordToSearchInDictionary].OrderByDescending(x => x.Length))
+                    {
+                        Console.WriteLine($"-{item}");
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            if (command == "List")
+            {
+                foreach (var kvp in wordsDefinitions.OrderBy(x => x.Key))
+                {
+                    Console.Write($"{kvp.Key} ");
+                }
+            }
+
+        }
+    }
+}
+
---
+++ 14FormerFinalExams/03(Demo)Final Exam-06April2019/(Demo)-06Apr2019/02.Deciphering/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^[d-z{}\|#]{1,}$";
+            string text = Console.ReadLine();
+            string[] textForReplacement = Console.ReadLine().Split();
+
+            if (Regex.IsMatch(text, pattern))
+            {
+                var collectionOfText = Regex.Matches(text, pattern);
+                string word = string.Empty;
+                foreach (Match item in collectionOfText)
+                {
+                    word = item.Value.ToString();
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char currentSymbol = word[i];
+                    int currentSymbolCode = (int)currentSymbol;
+                    int newSymbolCode = currentSymbolCode - 3;
+                    char newSymbolToAdd = (char)newSymbolCode;
+                    sb.Append(newSymbolToAdd);
+                }
+                string oldWord = textForReplacement[0];
+                string newWord = textForReplacement[1];
+                sb.Replace(oldWord, newWord);
+                string messageToPrint = sb.ToString();
+                Console.WriteLine(messageToPrint);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
