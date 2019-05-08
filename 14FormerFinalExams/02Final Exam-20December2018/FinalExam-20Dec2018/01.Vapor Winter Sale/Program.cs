460b32d14d8b7a85eb9ab2e8e4ec168697c10778
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-08 10:18:20 +0300

    Add Final Exam - 20 December 2018

---
+++ 14FormerFinalExams/02Final Exam-20December2018/FinalExam-20Dec2018/01.Vapor Winter Sale/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(", ");
+            Dictionary<string, double> gamePrice = new Dictionary<string, double>();
+            Dictionary<string, string> gameDlc = new Dictionary<string, string>();
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i].Contains("-"))
+                {
+                    string[] gamePriceTokens = tokens[i].Split("-");
+                    string game = gamePriceTokens[0];
+                    double price = double.Parse(gamePriceTokens[1]);
+                    if (!gamePrice.ContainsKey(game))
+                    {
+                        gamePrice[game] = price;
+                    }
+                }
+                if (tokens[i].Contains(":"))
+                {
+                    string[] gameDlcTokens = tokens[i].Split(":");
+                    string game = gameDlcTokens[0];
+                    string dlc = gameDlcTokens[1];
+                    if (gamePrice.ContainsKey(game))
+                    {
+                        gamePrice[game] *= 1.2;
+                        if (!gameDlc.ContainsKey(game))
+                        {
+                            gameDlc[game] = dlc;
+                        }
+                    }
+                }
+            }
+            Dictionary<string, double> gamesWithDlcPrice = new Dictionary<string, double>();
+            Dictionary<string, double> gamesWithoutPrice = new Dictionary<string, double>();
+            foreach (var kvp in gamePrice)
+            {
+                string game = kvp.Key;
+                double price = kvp.Value;
+                if (gameDlc.ContainsKey(game))
+                {
+                    gamesWithDlcPrice[game] = gamePrice[game] * 0.5;
+                }
+                else
+                {
+
+                    gamesWithoutPrice[game] = gamePrice[game] * 0.8;
+                }
+            }
+            foreach (var kvp in gamesWithDlcPrice.OrderBy(x => x.Value))
+            {
+                string game = kvp.Key;
+                double price = kvp.Value;
+                string dlc = gameDlc[game];
+                Console.WriteLine($"{game} - {dlc} - {price:f2}");
+            }
+            foreach (var kvp in gamesWithoutPrice.OrderByDescending(x => x.Value))
+            {
+                string game = kvp.Key;
+                double price = kvp.Value;
+                Console.WriteLine($"{game} - {price:f2}");
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/02Final Exam-20December2018/FinalExam-20Dec2018/02.Activation Keys/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split("&");
+            List<string> messages = new List<string>();
+            string pattern = @"[A-Za-z0-9]{16,25}";
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string text = tokens[i];
+                StringBuilder sb = new StringBuilder();
+
+                if (Regex.IsMatch(text, pattern) && text.Length == 16)
+                {
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        char symbol = text[j];
+
+                        if (char.IsDigit(symbol))
+                        {
+                            int digit = int.Parse(symbol.ToString());
+                            int newDigit = 9 - digit;
+                            string textToAdd = newDigit.ToString();
+                            sb.Append(textToAdd);
+                        }
+                        else if (char.IsLower(symbol))
+                        {
+                            string textToAdd = symbol.ToString().ToUpper();
+                            sb.Append(textToAdd);
+                        }
+                        else
+                        {
+                            sb.Append(symbol);
+                        }
+                        if ((j != text.Length - 1) && ((j + 1) % 4 == 0))
+                        {
+                            sb.Append("-");
+                        }
+                    }
+                    string textToAddToList = sb.ToString();
+                    messages.Add(textToAddToList);
+                }
+                if (Regex.IsMatch(text, pattern) && text.Length == 25)
+                {
+
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        char symbol = text[j];
+
+                        if (char.IsDigit(symbol))
+                        {
+                            int digit = int.Parse(symbol.ToString());
+                            int newDigit = 9 - digit;
+                            string textToAdd = newDigit.ToString();
+                            sb.Append(textToAdd);
+                        }
+                        else if (char.IsLower(symbol))
+                        {
+                            string textToAdd = symbol.ToString().ToUpper();
+                            sb.Append(textToAdd);
+                        }
+                        else
+                        {
+                            sb.Append(symbol);
+                        }
+                        if ((j != text.Length - 1) && ((j + 1) % 5 == 0))
+                        {
+                            sb.Append("-");
+                        }
+                    }
+                    string textToAddToList = sb.ToString();
+                    messages.Add(textToAddToList);
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", messages));
+        }
+    }
+}
