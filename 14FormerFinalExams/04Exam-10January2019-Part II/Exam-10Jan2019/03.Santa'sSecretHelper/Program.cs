6f215598c27d0afa7497f108e3f305aad6e44985
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-09 09:39:20 +0300

    Adding exercises from Retake Exam - 10 January 2019 Part II

---
+++ 14FormerFinalExams/04Exam-10January2019-Part II/Exam-10Jan2019/03.Santa'sSecretHelper/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            List<string> goodKids = new List<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < command.Length; i++)
+                    {
+                        char symbol = command[i];
+                        int asciiCode = (int)symbol;
+                        int newAsciiCode = asciiCode - key;
+                        char newSymbolToAdd = (char)newAsciiCode;
+                        sb.Append(newSymbolToAdd);
+                    }
+                    string decryptedText = sb.ToString();
+                    string pattern = @"([^@\-\!\:\>]{0,})@(?<name>[A-Za-z]{1,})([^@\-\!\:\>]{0,})!(?<type>[G|N]{1})!([^@\-\!\:\>]{0,})";
+                    if (Regex.IsMatch(decryptedText, pattern))
+                    {
+                        var matchesCollection = Regex.Matches(decryptedText, pattern);
+                        foreach (Match x in matchesCollection)
+                        {
+                            string name = x.Groups["name"].Value.ToString();
+                            string type = x.Groups["type"].Value.ToString();
+                            if (type == "G")
+                            {
+                                goodKids.Add(name);
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var x in goodKids)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/04Exam-10January2019-Part II/Exam-10Jan2019/04.Santa'sNewList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, int> toysCount = new Dictionary<string, int>();
+            Dictionary<string, int> childrenTotal = new Dictionary<string, int>();
+
+            while (command != "END")
+            {
+                string[] tokens = command.Split("->");
+                if (tokens[0] != "Remove")
+                {
+                    string name = tokens[0];
+                    string toy = tokens[1];
+                    int amount = int.Parse(tokens[2]);
+                    if (!toysCount.ContainsKey(toy))
+                    {
+                        toysCount[toy] = amount;
+                    }
+                    else
+                    {
+                        toysCount[toy] += amount;
+                    }
+                    if (!childrenTotal.ContainsKey(name))
+                    {
+                        childrenTotal[name] = amount;
+                    }
+                    else
+                    {
+                        childrenTotal[name] += amount;
+                    }
+                }
+                else
+                {
+                    string nameToRemove = tokens[1];
+                    childrenTotal.Remove(nameToRemove);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Children:");
+            foreach (var kvp in childrenTotal.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                int count = kvp.Value;
+                Console.WriteLine($"{name} -> {count}");
+            }
+            Console.WriteLine("Presents:");
+            foreach (var kvp in toysCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
