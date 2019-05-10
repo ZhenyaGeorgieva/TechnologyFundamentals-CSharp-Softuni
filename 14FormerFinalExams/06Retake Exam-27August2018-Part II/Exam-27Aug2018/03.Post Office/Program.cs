3219455bc790cdfcdcd9c540acc47aa592852255
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-10 08:18:09 +0300

    Adding exercises from Final Exam 27 August 2018

---
+++ 14FormerFinalExams/06Retake Exam-27August2018-Part II/Exam-27Aug2018/03.Post Office/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternCapitalLetters = @"([\#\$\%\*\&])(?<letters>[A-Z]{1,})\1";
+            string patternCodeLength = @"(?<code>[0-9]{2}):(?<length>[0-9]{2})";
+            string[] text = Console.ReadLine().Split(new char[] { '|' });
+
+            string firstPart = text[0];
+            string secondPart = text[1];
+            string thirdPart = text[2];
+
+            List<char> startingLetters = new List<char>();
+
+            if (Regex.IsMatch(firstPart, patternCapitalLetters))
+            {
+                var collectionLetters = Regex.Matches(firstPart, patternCapitalLetters);
+                foreach (Match item in collectionLetters)
+                {
+                    string matchedWord = item.Groups["letters"].Value.ToString();
+                    for (int i = 0; i < matchedWord.Length; i++)
+                    {
+                        startingLetters.Add(matchedWord[i]);
+                    }
+                }
+            }
+            Dictionary<char, int> charsAndLength = new Dictionary<char, int>();
+            if (Regex.IsMatch(secondPart, patternCodeLength))
+            {
+                var collectionLettersAndLength = Regex.Matches(secondPart, patternCodeLength);
+                foreach (Match item in collectionLettersAndLength)
+                {
+                    char letter = (char)int.Parse(item.Groups["code"].Value.ToString());
+                    int length = int.Parse(item.Groups["length"].Value.ToString());
+                    charsAndLength[letter] = length + 1;
+                }
+            }
+            string[] words = thirdPart.Split(new char[] { ' ' });
+            Dictionary<char, int> finalCharsLength = new Dictionary<char, int>();
+            foreach (var item in startingLetters)
+            {
+                char letterSought = item;
+                foreach (var kvp in charsAndLength)
+                {
+                    char currentLetter = kvp.Key;
+                    int currentLength = kvp.Value;
+                    if (letterSought == currentLetter)
+                    {
+                        finalCharsLength[letterSought] = currentLength;
+                    }
+                }
+            }
+            foreach (var kvp in finalCharsLength)
+            {
+                char firstLetter = kvp.Key;
+                int length = kvp.Value;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string currentWord = words[i];
+                    if (currentWord[0] == firstLetter
+                        && currentWord.Length == length)
+                    {
+                        Console.WriteLine(currentWord);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/06Retake Exam-27August2018-Part II/Exam-27Aug2018/04.Iron Girder/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> townBestTime = new Dictionary<string, int>();
+            Dictionary<string, int> townPassangers = new Dictionary<string, int>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Slide rule")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] tokens = command.Split(new string[] { ":", "->" }, StringSplitOptions.RemoveEmptyEntries);
+                    if (tokens[1] != "ambush")
+                    {
+                        string town = tokens[0];
+                        int time = int.Parse(tokens[1]);
+                        int passangers = int.Parse(tokens[2]);
+                        if (!townBestTime.ContainsKey(town))
+                        {
+                            townBestTime[town] = time;
+                        }
+                        else
+                        {
+                            if (time < townBestTime[town] && townBestTime[town] > 0)
+                            {
+                                townBestTime[town] = time;
+                            }
+                            if (townBestTime[town] == 0)
+                            {
+                                townBestTime[town] = time;
+                            }
+                        }
+                        if (!townPassangers.ContainsKey(town))
+                        {
+                            townPassangers[town] = passangers;
+                        }
+                        else
+                        {
+                            townPassangers[town] += passangers;
+                        }
+                    }
+                    else
+                    {
+                        string town = tokens[0];
+                        int passangers = int.Parse(tokens[2]);
+                        if (townPassangers.ContainsKey(town))
+                        {
+                            townPassangers[town] -= passangers;
+                        }
+                        if (townBestTime.ContainsKey(town))
+                        {
+                            townBestTime[town] = 0;
+                        }
+                    }
+                }
+            }
+            foreach (var kvp in townBestTime.OrderBy(x => x.Value).ThenBy(x => x.Key))
+            {
+                string town = kvp.Key;
+                int time = kvp.Value;
+                int passangers = townPassangers[town];
+                if (passangers > 0 && time > 0)
+                {
+                    Console.WriteLine($"{town} -> Time: {time} -> Passengers: {passangers}");
+                }
+            }
+        }
+    }
+}
