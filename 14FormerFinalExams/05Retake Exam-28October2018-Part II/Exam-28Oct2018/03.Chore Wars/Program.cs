c4f774e6dfa7d34ee76217a2173cf0ef811ef896
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-10 08:13:04 +0300

    Adding exercises from Final Exam 28 October 2018

---
+++ 14FormerFinalExams/05Retake Exam-28October2018-Part II/Exam-28Oct2018/03.Chore Wars/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternDishes = @"<(?<dishes>[a-z0-9]{1,})>";
+            string patternHouse = @"\[(?<house>[A-Z0-9]{1,})\]";
+            string patternLaundry = @"\{(?<laundry>[\S]{1,})\}";
+            int dishesTime = 0;
+            int laundryTime = 0;
+            int houseTime = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "wife is happy")
+                {
+                    break;
+                }
+                else
+                {
+                    if (Regex.IsMatch(command, patternDishes))
+                    {
+                        var dishesCollection = Regex.Matches(command, patternDishes);
+                        foreach (Match x in dishesCollection)
+                        {
+                            string dishesScrypt = x.Groups["dishes"].Value.ToString();
+                            for (int i = 0; i < dishesScrypt.Length; i++)
+                            {
+                                char symbol = dishesScrypt[i];
+                                if (char.IsDigit(symbol))
+                                {
+                                    int number = int.Parse(dishesScrypt[i].ToString());
+                                    dishesTime += number;
+                                }
+                            }
+                        }
+                    }
+                    if (Regex.IsMatch(command, patternHouse))
+                    {
+                        var houseCollection = Regex.Matches(command, patternHouse);
+                        foreach (Match x in houseCollection)
+                        {
+                            string houseScrypt = x.Groups["house"].Value.ToString();
+                            for (int i = 0; i < houseScrypt.Length; i++)
+                            {
+                                char symbol = houseScrypt[i];
+                                if (char.IsDigit(symbol))
+                                {
+                                    int number = int.Parse(houseScrypt[i].ToString());
+                                    houseTime += number;
+                                }
+                            }
+                        }
+                    }
+                    if (Regex.IsMatch(command, patternLaundry))
+                    {
+                        var laundryCollection = Regex.Matches(command, patternLaundry);
+                        foreach (Match x in laundryCollection)
+                        {
+                            string laundryScrypt = x.Groups["laundry"].Value.ToString();
+                            for (int i = 0; i < laundryScrypt.Length; i++)
+                            {
+                                char symbol = laundryScrypt[i];
+                                if (char.IsDigit(symbol))
+                                {
+                                    int number = int.Parse(laundryScrypt[i].ToString());
+                                    laundryTime += number;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Doing the dishes - {dishesTime} min.");
+            Console.WriteLine($"Cleaning the house - {houseTime} min.");
+            Console.WriteLine($"Doing the laundry - {laundryTime} min.");
+            Console.WriteLine($"Total - {dishesTime + laundryTime + houseTime} min.");
+        }
+    }
+}
---
+++ 14FormerFinalExams/05Retake Exam-28October2018-Part II/Exam-28Oct2018/04.MeTube Statistics/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, int> videoViews = new Dictionary<string, int>();
+            Dictionary<string, int> videoLikes = new Dictionary<string, int>();
+
+            while (command != "stats time")
+            {
+                if (command.Contains("-"))
+                {
+                    string[] tokens = command.Split("-");
+                    string video = tokens[0];
+                    int views = int.Parse(tokens[1]);
+                    if (!videoViews.ContainsKey(video))
+                    {
+                        videoViews[video] = views;
+                    }
+                    else
+                    {
+                        videoViews[video] += views;
+                    }
+                    if (!videoLikes.ContainsKey(video))
+                    {
+                        videoLikes[video] = 0;
+                    }
+                }
+                else if (command.Contains(":"))
+                {
+                    string[] tokens = command.Split(":");
+                    if (tokens[0] == "like")
+                    {
+                        if (videoViews.ContainsKey(tokens[1]))
+                        {
+                            videoLikes[tokens[1]] += 1;
+                        }
+                    }
+                    if (tokens[0] == "dislike")
+                    {
+                        if (videoViews.ContainsKey(tokens[1]))
+                        {
+                            videoLikes[tokens[1]] -= 1;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            string criterion = Console.ReadLine();
+            if (criterion == "by views")
+            {
+                foreach (var kvp in videoViews.OrderByDescending(x => x.Value))
+                {
+                    string video = kvp.Key;
+                    int views = kvp.Value;
+                    int likes = videoLikes[video];
+                    Console.WriteLine($"{video} - {views} views - {likes} likes");
+                }
+            }
+            if (criterion == "by likes")
+            {
+                foreach (var kvp in videoLikes.OrderByDescending(x => x.Value))
+                {
+                    string video = kvp.Key;
+                    int likes = kvp.Value;
+                    int views = videoViews[video];
+                    Console.WriteLine($"{video} - {views} views - {likes} likes");
+                }
+            }
+        }
+    }
+}
