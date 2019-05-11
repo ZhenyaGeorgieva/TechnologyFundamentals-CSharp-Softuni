efd16f6400421ad6ead3db937e38f51b203e065d
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-11 09:41:42 +0300

    Adding exercises from Final Exam 01 July 2018

---
+++ 14FormerFinalExams/07Exam-01July2018-Part II/Exam-01Jul2018/03.SoftUni Bar Income/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^([^\|\$\%\.]{0,})%(?<customer>[A-Z]{1}[a-z]{1,})%([^\|\$\%\.]{0,})<(?<product>[\w]{1,})>([^\|\$\%\.]{0,})\|(?<count>[0-9]{1,})\|([^0-9\|\$\%\.]{0,})(?<price>[0-9]{1,}[.]{0,}[0-9]{0,})\$([^\|\$\%\.]{0,})$";
+            double totalIncome = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end of shift")
+                {
+                    break;
+                }
+                else
+                {
+                    if (Regex.IsMatch(command, pattern))
+                    {
+                        var matchCollection = Regex.Matches(command, pattern);
+                        foreach (Match item in matchCollection)
+                        {
+                            string customer = item.Groups["customer"].Value.ToString();
+                            string product = item.Groups["product"].Value.ToString();
+                            int count = int.Parse(item.Groups["count"].Value.ToString());
+                            double price = double.Parse(item.Groups["price"].Value.ToString());
+                            double total = count * price;
+                            Console.WriteLine($"{customer}: {product} - {total:f2}");
+                            totalIncome += total;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
---
+++ 14FormerFinalExams/07Exam-01July2018-Part II/Exam-01Jul2018/04.SoftUniExamResults/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> languageSubmissions = new Dictionary<string, int>();
+            Dictionary<string, int> userMaxPoints = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "exam finished")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] tokens = command.Split(new char[] { '-' });
+                    if (tokens[1] != "banned")
+                    {
+                        string user = tokens[0];
+                        string language = tokens[1];
+                        int points = int.Parse(tokens[2]);
+                        if (!languageSubmissions.ContainsKey(language))
+                        {
+                            languageSubmissions[language] = 1;
+                        }
+                        else
+                        {
+                            languageSubmissions[language] += 1;
+                        }
+                        if (!userMaxPoints.ContainsKey(user))
+                        {
+                            userMaxPoints[user] = points;
+                        }
+                        else
+                        {
+                            if (points > userMaxPoints[user])
+                            {
+                                userMaxPoints[user] = points;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        string userToBan = tokens[0];
+                        if (userMaxPoints.ContainsKey(userToBan))
+                        {
+                            userMaxPoints.Remove(userToBan);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Results:");
+            foreach (var kvp in userMaxPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                int points = kvp.Value;
+                Console.WriteLine($"{name} | {points}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var kvp in languageSubmissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string language = kvp.Key;
+                int value = kvp.Value;
+                Console.WriteLine($"{language} - {value}");
+            }
+        }
+    }
+}
