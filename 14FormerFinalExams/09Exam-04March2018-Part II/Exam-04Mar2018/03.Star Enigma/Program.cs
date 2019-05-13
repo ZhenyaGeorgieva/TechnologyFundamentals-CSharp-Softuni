69eae6cd1918d9c3bf11316678d1d6f63510f74d
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-13 09:23:21 +0300

    Adding exercises from Final Exam 04 March 2018

---
+++ 14FormerFinalExams/09Exam-04March2018-Part II/Exam-04Mar2018/03.Star Enigma/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _11StarEnigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> attackedPlanets = new List<string>();
+            List<string> destroyedPlanets = new List<string>();
+            int messagesCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < messagesCount; i++)
+            {
+                string message = Console.ReadLine();
+                int specialLetters = 0;
+                for (int j = 0; j < message.Length; j++)
+                {
+                    char currentLetter = message[j];
+                    if (currentLetter == 'S'
+                        || currentLetter == 'T'
+                        || currentLetter == 'A'
+                        || currentLetter == 'R'
+                        || currentLetter == 's'
+                        || currentLetter == 't'
+                        || currentLetter == 'a'
+                        || currentLetter == 'r')
+                    {
+                        specialLetters++;
+                    }
+                }
+                string decrypted = string.Empty;
+                for (int y = 0; y < message.Length; y++)
+                {
+                    int currentLetter = (int)message[y];
+                    char newLetter = (char)(currentLetter - specialLetters);
+                    decrypted += newLetter;
+                }
+                string regex = @"@(?<planet>[A-Za-z]{1,})([^@\-\!\:>]{0,}):(?<population>[0-9]{1,})([^@\-\!\:>]{0,})!(?<attack>[A|D])!([^@\-\!\:>]{0,})->(?<soldiers>[0-9]{1,})";
+                if (Regex.IsMatch(decrypted, regex))
+                {
+                    var matchCombination = Regex.Matches(decrypted, regex);
+                    foreach (Match x in matchCombination)
+                    {
+                        string planet = x.Groups["planet"].Value.ToString();
+                        string type = x.Groups["attack"].Value.ToString();
+                        if (type == "A")
+                        {
+                            attackedPlanets.Add(planet);
+                        }
+                        else
+                        {
+                            destroyedPlanets.Add(planet);
+                        }
+                    }
+                }
+            }
+            var attackedToPrint = attackedPlanets.OrderBy(x => x);
+            var destroyedToPrint = destroyedPlanets.OrderBy(x => x);
+            Console.WriteLine($"Attacked planets: { attackedPlanets.Count}");
+            foreach (var x in attackedToPrint)
+            {
+                Console.WriteLine($"-> {x}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+            foreach (var x in destroyedToPrint)
+            {
+                Console.WriteLine($"-> {x}");
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/09Exam-04March2018-Part II/Exam-04Mar2018/04.ForceBook/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, List<string>> sideUsers = new Dictionary<string, List<string>>();
+            while (command != "Lumpawaroo")
+            {
+                if (command.Contains("|"))
+                {
+                    string[] tokens = command.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string side = tokens[0].Trim();
+                    string user = tokens[1].Trim();
+                    if (!sideUsers.ContainsKey(side))
+                    {
+                        sideUsers[side] = new List<string>();
+                    }
+                    if (!sideUsers[side].Contains(user)
+                            && sideUsers.Values.Any(x => x.Contains(user)) == false)
+                    {
+                        sideUsers[side].Add(user);
+                    }
+                }
+                if (command.Contains("->"))
+                {
+                    string[] tokens = command.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string side = tokens[1].Trim();
+                    string user = tokens[0].Trim();
+                    bool userFound = false;
+                    foreach (var kvp in sideUsers)
+                    {
+                        string side1 = kvp.Key;
+                        var listOfUsersSide1 = kvp.Value;
+                        foreach (var users in listOfUsersSide1)
+                        {
+                            if (users == user)
+                            {
+                                listOfUsersSide1.Remove(user);
+                                userFound = true;
+                                if (sideUsers.ContainsKey(side))
+                                {
+                                    sideUsers[side].Add(user);
+                                    Console.WriteLine($"{user} joins the {side} side!");
+                                    break;
+                                }
+                                else
+                                {
+                                    sideUsers[side] = new List<string>();
+                                    sideUsers[side].Add(user);
+                                    Console.WriteLine($"{user} joins the {side} side!");
+                                    break;
+                                }
+
+                            }
+                        }
+                        if (userFound)
+                        {
+                            break;
+                        }
+                    }
+                    if (!userFound)
+                    {
+                        if (sideUsers.ContainsKey(side))
+                        {
+                            sideUsers[side].Add(user);
+                            Console.WriteLine($"{user} joins the {side} side!");
+                        }
+                        else
+                        {
+                            sideUsers[side] = new List<string>();
+                            sideUsers[side].Add(user);
+                            Console.WriteLine($"{user} joins the {side} side!");
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var x in sideUsers.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                string side = x.Key;
+                int count = x.Value.Count;
+                if (count != 0)
+                {
+                    Console.WriteLine($"Side: {side}, Members: { count}");
+                    foreach (var names in x.Value.OrderBy(y => y))
+                    {
+                        Console.WriteLine($"! {names}");
+                    }
+                }
+            }
+        }
+    }
+}
+
