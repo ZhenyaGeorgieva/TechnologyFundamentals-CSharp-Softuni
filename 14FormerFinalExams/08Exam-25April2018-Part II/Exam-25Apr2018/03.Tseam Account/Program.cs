97f7a157dbed541f2e8ad499d7fd5c1a375efac0
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-13 09:17:35 +0300

    Adding exercises from Final Exam 25 April 2018

---
+++ 14FormerFinalExams/08Exam-25April2018-Part II/Exam-25Apr2018/03.Tseam Account/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09TseamAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gameList = Console.ReadLine().Split(new char[] { ' ' }).ToList();
+            string command = Console.ReadLine();
+
+
+            while (command != "Play!")
+            {
+                string[] tokens = command.Split(new char[] { ' ' }).ToArray();
+                string action = tokens[0];
+                if (action == "Install")
+                {
+                    string game = tokens[1];
+                    if (!gameList.Contains(game))
+                    {
+                        gameList.Add(game);
+                    }
+                }
+                else if (action == "Uninstall")
+                {
+                    gameList.Remove(tokens[1]);
+                }
+                else if (action == "Update")
+                {
+                    string game = tokens[1];
+                    if (gameList.Contains(game))
+                    {
+                        gameList.Remove(game);
+                        gameList.Add(game);
+                    }
+                }
+                else if (action == "Expansion")
+                {
+                    string[] tokens1 = tokens[1].Split(new char[] { '-' }).ToArray();
+
+                    string game = tokens1[0];
+                    string expansion = tokens1[1];
+                    if (gameList.Contains(game))
+                    {
+                        int indexOfGame = gameList.IndexOf(game);
+                        if (indexOfGame != gameList.Count - 1)
+                        {
+                            gameList.Insert(indexOfGame + 1, $"{game}:{expansion}");
+                        }
+                        else
+                        {
+                            gameList.Add($"{game}:{expansion}");
+                        }
+
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", gameList));
+        }
+    }
+}
---
+++ 14FormerFinalExams/08Exam-25April2018-Part II/Exam-25Apr2018/04.MOBA Challenger/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10MobaChallenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> playerPositionSkill = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> playerTotalSkill = new Dictionary<string, int>();
+
+            string command = Console.ReadLine();
+            while (command != "Season end")
+            {
+                if (command.Contains("->"))
+                {
+                    string[] tokens = command.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string player = tokens[0].Trim();
+                    string position = tokens[1].Trim();
+                    int skill = int.Parse(tokens[2]);
+                    int skillToAdd = 0;
+                    if (!playerPositionSkill.ContainsKey(player))
+                    {
+                        playerPositionSkill[player] = new Dictionary<string, int>();
+                        playerPositionSkill[player][position] = skill;
+                    }
+                    else
+                    {
+                        if (!playerPositionSkill[player].ContainsKey(position))
+                        {
+                            playerPositionSkill[player][position] = skill;
+                            skillToAdd = skill;
+
+                        }
+                        else
+                        {
+                            if (skill > playerPositionSkill[player][position])
+                            {
+                                playerPositionSkill[player][position] = skill;
+                                skillToAdd = skill - playerPositionSkill[player][position];
+                            }
+                        }
+                    }
+                    if (!playerTotalSkill.ContainsKey(player))
+                    {
+                        playerTotalSkill[player] = skill;
+
+                    }
+                    else
+                    {
+                        playerTotalSkill[player] += skillToAdd;
+                    }
+                }
+                else
+                {
+                    string[] tokens = command.Split(new string[] { "vs" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string player1 = tokens[0].Trim();
+                    string player2 = tokens[1].Trim();
+                    bool isTherePositionInCommon = false;
+                    string playerToRemove = string.Empty;
+                    if (playerPositionSkill.ContainsKey(player1) && playerPositionSkill.ContainsKey(player2))
+                    {
+                        foreach (var kvp in playerPositionSkill[player1])
+                        {
+                            string positionPlayer1 = kvp.Key;
+                            foreach (var kvp1 in playerPositionSkill[player2])
+                            {
+                                string positionPlayer2 = kvp1.Key;
+                                if (positionPlayer1 == positionPlayer2)
+                                {
+                                    isTherePositionInCommon = true;
+                                    break;
+                                }
+                            }
+                            if (isTherePositionInCommon)
+                            {
+                                break;
+                            }
+                        }
+                        if (isTherePositionInCommon)
+                        {
+                            int totalSkillPlayer1 = playerTotalSkill[player1];
+                            int totalSkillPlayer2 = playerTotalSkill[player2];
+                            if (totalSkillPlayer1 > totalSkillPlayer2)
+                            {
+                                playerToRemove = player2;
+                            }
+                            else if (totalSkillPlayer2 > totalSkillPlayer1)
+                            {
+                                playerToRemove = player1;
+                            }
+                        }
+                    }
+                    if (playerToRemove != string.Empty)
+                    {
+                        playerPositionSkill.Remove(playerToRemove);
+                        playerTotalSkill.Remove(playerToRemove);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            foreach (var kvp in playerTotalSkill.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                int total = kvp.Value;
+                Console.WriteLine($"{name}: {total} skill");
+                foreach (var kvp1 in playerPositionSkill[name].OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    string position = kvp1.Key;
+                    int skill = kvp1.Value;
+                    Console.WriteLine($"- {position} <::> {skill}");
+                }
+            }
+        }
+    }
+}
