9ba4e9f41c03a47ca637ff052abe4589d28174ef
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-14 10:29:48 +0300

    Adding exercises from Exam - 09 July 2017 Part 2

---
+++ 14FormerFinalExams/11Exam-09July2017-Part II/Exam-09Jul2017/03.Regexmon/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _20Regexmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string patternDidi = @"[^A-Za-z-]{1,}";
+            string patternBojo = @"[A-Za-z]{1,}-[A-Za-z]{1,}";
+            string didiMatch = string.Empty;
+            string bojoMatch = string.Empty;
+            while (true)
+            {
+                if (Regex.IsMatch(command, patternDidi))
+                {
+
+                    var didiCollection = Regex.Matches(command, patternDidi);
+                    foreach (Match x in didiCollection)
+                    {
+                        didiMatch = x.Value.ToString();
+                        Console.WriteLine(didiMatch);
+                        break;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+                int indexDidiMatch = command.IndexOf(didiMatch);
+                command = command.Substring(indexDidiMatch + didiMatch.Length);
+
+                if (Regex.IsMatch(command, patternBojo))
+                {
+                    var bojoCollection = Regex.Matches(command, patternBojo);
+                    foreach (Match x in bojoCollection)
+                    {
+                        bojoMatch = x.Value.ToString();
+                        Console.WriteLine(bojoMatch);
+                        break;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+                int indexBojoMatch = command.IndexOf(bojoMatch);
+                command = command.Substring(indexBojoMatch + bojoMatch.Length);
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/11Exam-09July2017-Part II/Exam-09Jul2017/04.Pokemon Evolution/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _22.PokemonEvolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> pokemonEvolutionIndex = new Dictionary<string, List<string>>();
+            string input = Console.ReadLine();
+            while (input != "wubbalubbadubdub")
+            {
+                if (input.Contains("->"))
+                {
+                    string[] command = input
+                        .Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                    string pokemon = command[0].Trim();
+                    string evolution = command[1].Trim();
+                    int index = int.Parse(command[2].Trim());
+
+                    if (!pokemonEvolutionIndex.ContainsKey(pokemon))
+                    {
+                        pokemonEvolutionIndex[pokemon] = new List<string>();
+                        string stringToAdd = evolution + ">" + index;
+                        pokemonEvolutionIndex[pokemon].Add(stringToAdd);
+                    }
+                    else
+                    {
+                        string stringToAdd = evolution + ">" + index;
+                        pokemonEvolutionIndex[pokemon].Add(stringToAdd);
+                    }
+                }
+                else
+                {
+                    string pokemon = input;
+                    if (pokemonEvolutionIndex.ContainsKey(pokemon))
+                    {
+                        Console.WriteLine($"# {pokemon}");
+                        foreach (var x in pokemonEvolutionIndex[pokemon])
+                        {
+                            string[] evolutionAndValue = x.Split(new char[] { '>' });
+                            string evolution = evolutionAndValue[0];
+                            int value = int.Parse(evolutionAndValue[1]);
+                            Console.WriteLine($"{ evolution} <-> { value}");
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var kvp in pokemonEvolutionIndex)
+            {
+                string pokemon = kvp.Key;
+                Console.WriteLine($"# {pokemon}");
+                var evolutionAndValue = kvp.Value;
+                foreach (var item in evolutionAndValue.OrderByDescending(x => int.Parse(x.Split(new char[] { '>' })[1])))
+                {
+                    string[] tokens = item.Split(new char[] { '>' });
+                    string evolution = tokens[0];
+                    int value = int.Parse(tokens[1]);
+                    Console.WriteLine($"{ evolution} <-> { value}");
+                }
+            }
+        }
+    }
+}
+
