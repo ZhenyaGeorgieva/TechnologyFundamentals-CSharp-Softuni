52942840e4639abe5d774207a1d040f8d767fd5a
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-14 10:24:06 +0300

    Adding exercises from Final Exam 05 November 2017 Part II

---
+++ 14FormerFinalExams/10Exam-05November2017-Part II/Exam-05Nov2017/03.Anonymous Vox/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _13.AnonimousVox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"([A-Za-z]{1,})(?<placeholder>[\S]{1,})\1";
+            string text = Console.ReadLine();
+            List<string> placeholders = new List<string>();
+            var placeholdersCollection = Regex.Matches(text, regex);
+            string text1 = string.Empty;
+            if (Regex.IsMatch(text, regex))
+            {
+                foreach (Match x in placeholdersCollection)
+                {
+                    placeholders.Add(x.Groups["placeholder"].Value);
+                }
+            }
+            string wordsString = Console.ReadLine();
+            string regexWords = @"([{]{1})(?<words>[^{}]{1,})([}]{1})";
+            List<string> words = new List<string>();
+            var wordsCollection = Regex.Matches(wordsString, regexWords);
+            if (Regex.IsMatch(wordsString, regexWords))
+            {
+                foreach (Match w in wordsCollection)
+                {
+                    words.Add(w.Groups["words"].Value);
+                }
+            }
+            if (placeholders.Count <= words.Count)
+            {
+
+                for (int i = 0; i < placeholders.Count; i++)
+                {
+                    int indexOf = text.IndexOf(placeholders[i]);
+                    text = text.Remove(indexOf, placeholders[i].Length);
+                    text = text.Insert(indexOf, words[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < words.Count; i++)
+                {
+                    int indexOf = text.IndexOf(placeholders[i]);
+                    text = text.Remove(indexOf, placeholders[i].Length);
+                    text = text.Insert(indexOf, words[i]);
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 14FormerFinalExams/10Exam-05November2017-Part II/Exam-05Nov2017/04.Anonymous Cache/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14.AnonimousCashe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> setTotalSize = new Dictionary<string, long>();
+            Dictionary<string, List<string>> setKeys = new Dictionary<string, List<string>>();
+            Dictionary<string, Dictionary<string, long>> cashe = new Dictionary<string, Dictionary<string, long>>();
+
+            string command = Console.ReadLine();
+            while (command != "thetinggoesskrra")
+            {
+                if (!(command.Contains("|") && command.Contains("->")))
+                {
+                    string set = command;
+                    if (!setTotalSize.ContainsKey(set))
+                    {
+                        setTotalSize[set] = 0;
+                    }
+                    if (!setKeys.ContainsKey(set))
+                    {
+                        setKeys[set] = new List<string>();
+                    }
+                    if (cashe.ContainsKey(set))
+                    {
+                        foreach (var kvp in cashe[set])
+                        {
+                            string key = kvp.Key;
+                            long value = kvp.Value;
+                            setTotalSize[set] += value;
+                            if (!setKeys[set].Contains(key))
+                            {
+                                setKeys[set].Add(key);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string[] tokens = command.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                    string key = tokens[0];
+                    string[] sizeAndSet = tokens[1].Split(new string[] { " | " }, StringSplitOptions.RemoveEmptyEntries);
+                    long size = long.Parse(sizeAndSet[0]);
+                    string set = sizeAndSet[1];
+                    if (setKeys.ContainsKey(set) && setTotalSize.ContainsKey(set))
+                    {
+                        setKeys[set].Add(key);
+                        setTotalSize[set] += size;
+                    }
+                    else
+                    {
+                        if (!cashe.ContainsKey(set))
+                        {
+                            cashe[set] = new Dictionary<string, long>();
+                            cashe[set][key] = size;
+                        }
+                        else
+                        {
+                            if (!cashe[set].ContainsKey(key))
+                            {
+                                cashe[set][key] = size;
+                            }
+                            else
+                            {
+                                cashe[set][key] += size;
+                            }
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            string bestSet = string.Empty;
+            foreach (var kvp in setTotalSize.OrderByDescending(x => x.Value))
+            {
+
+                bestSet = kvp.Key;
+                break;
+            }
+            if (bestSet != string.Empty)
+            {
+                Console.WriteLine($"Data Set: {bestSet}, Total Size: {setTotalSize[bestSet]}");
+
+                foreach (var x in setKeys[bestSet])
+                {
+                    Console.WriteLine($"$.{x}");
+                }
+            }
+        }
+    }
+}
