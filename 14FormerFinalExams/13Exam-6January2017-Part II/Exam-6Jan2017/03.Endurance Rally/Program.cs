b807f002c4685009b75801efd66d80eebdc70657
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-15 08:48:29 +0300

    Adding exercises from Retake Exam - Part 2 - 6 January 2017

---
+++ 14FormerFinalExams/13Exam-6January2017-Part II/Exam-6Jan2017/03.Endurance Rally/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _25EnduranceRally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            double[] track = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            List<int> indexes = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string currentName = names[i];
+                int fuel1 = (int)currentName[0];
+                double fuel = fuel1;
+                for (int y = 0; y < track.Length; y++)
+                {
+                    if (indexes.Contains(y))
+                    {
+                        fuel += track[y];
+                    }
+                    else
+                    {
+                        fuel -= track[y];
+                    }
+                    if (fuel <= 0)
+                    {
+                        Console.WriteLine($"{currentName} - reached {y}");
+                        break;
+                    }
+                }
+                if (fuel > 0)
+                {
+                    Console.WriteLine($"{currentName} - fuel left {fuel:f2}");
+                }
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/13Exam-6January2017-Part II/Exam-6Jan2017/Exam-6Jan2017/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam_6Jan2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
