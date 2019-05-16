6eed4ebd6ab0a83733a053dcbf9ee10b769b2ba1
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-16 08:37:36 +0300

    Adding exercises from Exam - Part 2 - 23 October 2016

---
+++ 14FormerFinalExams/14Exam-23October2016-Part II/Exam-23Oct2016/03.Nether Realms/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _27NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> demonHealth = new Dictionary<string, int>();
+            Dictionary<string, double> demonDamage = new Dictionary<string, double>();
+
+            string patternHealth = @"([^0-9\+\-\*\/\.]{1})";
+            string patternDamage = @"([\+|\-]{0,1}[0-9]{1,}[\.]{0,1}[0-9]{0,})";
+            string patternActions = @"([\*|\/]{1})";
+
+            string[] tokens = Console.ReadLine().Split(new char[] { ' ', '\t', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string name = tokens[i];
+                int health = 0;
+                if (Regex.IsMatch(name, patternHealth))
+                {
+                    var symbolsHealthCollection = Regex.Matches(name, patternHealth);
+                    foreach (Match item in symbolsHealthCollection)
+                    {
+                        char symbol = char.Parse(item.Value.ToString());
+                        int symbolValue = (int)symbol;
+                        health += symbolValue;
+                    }
+                }
+                double damage = 0;
+                if (Regex.IsMatch(name, patternDamage))
+                {
+                    var symbolsDamageCollection = Regex.Matches(name, patternDamage);
+                    foreach (Match item in symbolsDamageCollection)
+                    {
+                        double symbol = double.Parse(item.Value.ToString());
+                        damage += symbol;
+                    }
+                }
+                if (Regex.IsMatch(name, patternActions))
+                {
+                    var actionsCollection = Regex.Matches(name, patternActions);
+                    foreach (Match x in actionsCollection)
+                    {
+                        if (x.Value.ToString() == "*")
+                        {
+                            damage *= 2;
+                        }
+                        if (x.Value.ToString() == "/")
+                        {
+                            damage /= 2;
+                        }
+                    }
+                }
+                demonDamage[name] = damage;
+                demonHealth[name] = health;
+            }
+            foreach (var kvp in demonHealth.OrderBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                int health = kvp.Value;
+                double damage = demonDamage[name];
+                Console.WriteLine($"{name} - {health} health, {damage:f2} damage");
+            }
+        }
+    }
+}
---
+++ 14FormerFinalExams/14Exam-23October2016-Part II/Exam-23Oct2016/04.Roli The Coder/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _28RoliTheCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> idName = new Dictionary<string, string>();
+            Dictionary<string, List<string>> nameParticipants = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Time for Code")
+                {
+                    break;
+                }
+                else
+                {
+                    if (command.Contains("#"))
+                    {
+                        string[] tokens = command.Split(new char[] { '#', '\t', '\n', '\r' });
+                        string id = tokens[0].Trim();
+
+                        if (tokens[1].Contains("@"))
+                        {
+                            string[] nameAndParticipants = tokens[1].Split(new char[] { '@', '\t', '\n', '\r' });
+                            string name = nameAndParticipants[0].Trim();
+                            if (!idName.ContainsKey(id))
+                            {
+                                idName[id] = name;
+                                if (!nameParticipants.ContainsKey(name))
+                                {
+                                    nameParticipants[name] = new List<string>();
+                                }
+                            }
+                            else
+                            {
+                                if (name != idName[id])
+                                {
+                                    continue;
+                                }
+                            }
+                            for (int i = 1; i < nameAndParticipants.Length; i++)
+                            {
+                                string participant = nameAndParticipants[i].Trim();
+                                if (!nameParticipants.ContainsKey(name))
+                                {
+                                    nameParticipants[name] = new List<string>();
+                                    nameParticipants[name].Add(participant);
+                                }
+                                else
+                                {
+                                    if (!nameParticipants[name].Contains(participant))
+                                    {
+                                        nameParticipants[name].Add(participant);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string name = tokens[1].Trim();
+                            if (!idName.ContainsKey(id))
+                            {
+                                idName[id] = name;
+                                if (!nameParticipants.ContainsKey(name))
+                                {
+                                    nameParticipants[name] = new List<string>();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var kvp in nameParticipants.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                string name = kvp.Key;
+                var participants = kvp.Value;
+                Console.WriteLine($"{name} - {participants.Count}");
+                foreach (var x in participants.OrderBy(x => x))
+                {
+                    Console.WriteLine($"@{x}");
+                }
+            }
+        }
+    }
+}
