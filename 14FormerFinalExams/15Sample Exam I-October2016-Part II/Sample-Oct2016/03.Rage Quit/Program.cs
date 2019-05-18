49dc2433cfdc334a3e7d603af1117f46cc697893
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-05-18 08:44:22 +0300

    Adding exercises from Sample Exam I - October 2016

---
+++ 14FormerFinalExams/15Sample Exam I-October2016-Part II/Sample-Oct2016/03.Rage Quit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _31RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialText = Console.ReadLine();
+            string pattern = @"(?<word>[^0-9]{1,})(?<count>[0-9]{1,})";
+
+            StringBuilder sb = new StringBuilder();
+
+            if (Regex.IsMatch(initialText, pattern))
+            {
+                var collectionA = Regex.Matches(initialText, pattern);
+                foreach (Match x in collectionA)
+                {
+                    string textToRepeat = x.Groups["word"].Value.ToString().ToUpper();
+                    int repeatTimes = int.Parse(x.Groups["count"].Value.ToString());
+                    for (int i = 0; i < repeatTimes; i++)
+                    {
+                        sb.Append(textToRepeat);
+                    }
+                }
+            }
+            int uniqueSymbols = sb.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ 14FormerFinalExams/15Sample Exam I-October2016-Part II/Sample-Oct2016/04.Files/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, long>> rootFileSize = new Dictionary<string, Dictionary<string, long>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                if (command.Contains('\\'))
+                {
+                    string[] path = command.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                    string root = path[0];
+                    int lastIndex = command.LastIndexOf('\\');
+                    string fileExtentionSize = command.Substring(lastIndex + 1);
+                    if (fileExtentionSize.Contains(';'))
+                    {
+                        string[] fileNameSize = fileExtentionSize.Split(new char[] { ';' });
+                        string fileNameExtention = fileNameSize[0];
+                        long fileSize = long.Parse(fileNameSize[1]);
+                        if (!rootFileSize.ContainsKey(root))
+                        {
+                            rootFileSize[root] = new Dictionary<string, long>();
+                            rootFileSize[root][fileNameExtention] = fileSize;
+                        }
+                        else
+                        {
+                            if (!rootFileSize[root].ContainsKey(fileNameExtention))
+                            {
+                                rootFileSize[root][fileNameExtention] = fileSize;
+                            }
+                            else
+                            {
+                                rootFileSize[root][fileNameExtention] = fileSize;
+                            }
+                        }
+                    }
+                }
+            }
+            string[] extractionCommand = Console.ReadLine().Split(new char[] { ' ' });
+            string rootSearch = extractionCommand[2];
+            string fileExtentionSearch = extractionCommand[0];
+
+            if (!rootFileSize.ContainsKey(rootSearch))
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                var filesInRoot = rootFileSize[rootSearch];
+                bool found = false;
+                foreach (var kvp in filesInRoot.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    if (kvp.Key.Contains('.'))
+                    {
+                        int lastIndexOfDot = kvp.Key.LastIndexOf('.');
+                        string extention = kvp.Key.Substring(lastIndexOfDot + 1);
+                        string name = kvp.Key.Remove(lastIndexOfDot);
+                        long size = kvp.Value;
+                        if (extention == fileExtentionSearch)
+                        {
+                            found = true;
+                            Console.WriteLine($"{name}.{extention} - {size} KB");
+                        }
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
